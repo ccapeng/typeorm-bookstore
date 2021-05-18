f1969a37815e94cc73812b48682f811284702e89
Alex Peng <ccapeng@yahoo.com>
2021-05-17 20:11:33 -0400

    cors

--- src/app.ts
+++ src/app.ts
@@ -64,4 +64,15 @@ app.use(function notFoundHandler(_req, res: express.Response) {
   return res.status(404).send({ message: "Not Found" });
 });
 
+// CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+  }
+  next();
+});
+
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,6 @@ import app from './app';
 
 dotenv.config({});
 
-// CORS
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-      return res.status(200).json({});
-  }
-  next();
-});
-
 // Start the application by listening to specific port
 const port = Number(process.env.PORT || process.env.PORT || 8080);
 app.listen(port, () => {
